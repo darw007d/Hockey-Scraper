f4b76ddbd76b765290b13f34ce3a6471aa65b264
Darwood <pierresamson@gmail.com>
2024-10-15 14:15:26 +0200

    Update json_schedule.py

--- hockey_scraper/nhl/json_schedule.py
+++ hockey_scraper/nhl/json_schedule.py
@@ -83,6 +83,7 @@ def scrape_schedule(date_from, date_to, preseason=False, not_over=False):
     tds = list(map(int, date_to.split("-")))
     tdate_est = datetime(tds[0], tds[1], tds[2], 23, 59, tzinfo=est)
     sea_date = datetime(2024,7,1,00,00, tzinfo=est)
+    today_date = datetime(year(date.today()),month(date.today()),day(date.today()),00,00, tzinfo=est)
 
     schedule = []
     schedule_json = chunk_schedule_calls(date_from, date_to)
